efd249ea02f0ca41118438bb80fd2f9c241e8ba8
Daniel Kloeters <dkloeters@hax-solutions.de>
2017-11-08 09:55:56 +0100

    raised autoform dependency version to 6.0.0

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'q42:autoform-markdown',
+  name: 'd3k4y:autoform-markdown',
   version: '1.0.0',
   summary: 'A simple autoform markdown with preview using perak:markdown',
   git: 'https://github.com/Q42/meteor-autoform-markdown',
@@ -9,7 +9,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
   api.use(['templating', 'reactive-var'], 'client');
-  api.use(['perak:markdown@1.0.5', 'aldeed:autoform@5.4.1'], 'client');
+  api.use(['perak:markdown@1.0.5', 'aldeed:autoform@6.0.0 || 5.0.0'], 'client');
   api.addFiles([
     'markdown.html',
     'markdown.css',
@@ -21,5 +21,5 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('q42:autoform-markdown');
+  api.use('d3k4y:autoform-markdown');
 });
