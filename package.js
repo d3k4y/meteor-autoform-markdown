b3a62f86887a85d04691307da2b7fa69395b9529
Daniel Kloeters <dkloeters@hax-solutions.de>
2017-11-08 10:01:54 +0100

    raised package minor version due to dependency change

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'd3k4y:autoform-markdown',
-  version: '1.0.0',
+  version: '1.1.0',
   summary: 'A simple autoform markdown with preview using perak:markdown',
   git: 'https://github.com/Q42/meteor-autoform-markdown',
   documentation: 'README.md'
